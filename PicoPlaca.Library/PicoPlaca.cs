4cd33eee7874d41e5b18f300db3cd928ed168e0c
Maria Lopez <mariajoselopezteran@outlook.com>
2020-02-20 10:14:37 -0500

    Verify Paque number
    Method to verify that plaque is valid

---
+++ PicoPlaca.Library/PicoPlaca.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace PicoPlaca.Library
+{
+    public class PicoPlaca
+    {
+        public static bool IsPlaqueValid(string plaque)
+        {
+            bool isPlaqueValid = false;
+            if (plaque.Length >= 6 || plaque.Length < 8)
+            {
+                int numbers = Regex.Match(plaque, @"\d+").Value.Length;
+                int letters = plaque.Length - numbers;
+
+                if (letters == 3 && (numbers >= 3 || numbers < 5))
+                {
+                    //First 3 Digits are letters
+                    if (Regex.IsMatch(plaque.Substring(0, 2), @"^[a-zA-Z]+$"))
+                        isPlaqueValid = true;
+
+                    //Last Digits are letters
+                    if (Regex.IsMatch(plaque.Substring(3, numbers), @"\d+"))
+                        isPlaqueValid = true;
+                }
+            }
+            return isPlaqueValid;
+        }
+    }
+}
---
+++ PicoPlaca.Test/PicoPlacaTest.cs
@@ -0,0 +1,125 @@
+using NUnit.Framework;
+using PicoPlaca.Library;
+
+namespace PicoPlaca.Test
+{
+    public class PicoPlacaTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void PicoPlaca_WithPlaqueLength6_ReturnsTrue()
+        {
+            //Assert
+            string plaque = "IBB355";
+            bool actual = true;
+
+            //Act
+            var output = PicoPlaca.Library.PicoPlaca.IsPlaqueValid(plaque);
+
+            //Arrange
+            Assert.AreEqual(actual,output);
+        }
+
+        [Test]
+        public void PicoPlaca_WithPlaqueLength7_ReturnsTrue()
+        {
+            //Assert
+            string plaque = "IBB3555";
+            bool actual = true;
+
+            //Act
+            var output = PicoPlaca.Library.PicoPlaca.IsPlaqueValid(plaque);
+
+            //Arrange
+            Assert.AreEqual(actual, output);
+        }
+
+        [Test]
+        public void PicoPlaca_WithPlaqueAllNumbers_ReturnsFalse()
+        {
+            //Assert
+            string plaque = "3333555";
+            bool actual = false;
+
+            //Act
+            var output = PicoPlaca.Library.PicoPlaca.IsPlaqueValid(plaque);
+
+            //Arrange
+            Assert.AreEqual(actual, output);
+        }
+
+        [Test]
+        public void PicoPlaca_WithPlaqueAllLetters_ReturnsFalse()
+        {
+            //Assert
+            string plaque = "AAAAAA";
+            bool actual = false;
+
+            //Act
+            var output = PicoPlaca.Library.PicoPlaca.IsPlaqueValid(plaque);
+
+            //Arrange
+            Assert.AreEqual(actual, output);
+        }
+
+        [Test]
+        public void PicoPlaca_WithPlaqueStarts3letters_ReturnsTrue()
+        {
+            //Assert
+            string plaque = "AAA333";
+            bool actual = true;
+
+            //Act
+            var output = PicoPlaca.Library.PicoPlaca.IsPlaqueValid(plaque);
+
+            //Arrange
+            Assert.AreEqual(actual, output);
+        }
+
+        [Test]
+        public void PicoPlaca_WithPlaqueLastDigitsLetters_ReturnsFalse()
+        {
+            //Assert
+            string plaque = "AAAAA";
+            bool actual = false;
+
+            //Act
+            var output = PicoPlaca.Library.PicoPlaca.IsPlaqueValid(plaque);
+
+            //Arrange
+            Assert.AreEqual(actual, output);
+        }
+
+        [Test]
+        public void PicoPlaca_WithPlaqueLast3Numbers_ReturnsTrue()
+        {
+            //Assert
+            string plaque = "AAA222";
+            bool actual = true;
+
+            //Act
+            var output = PicoPlaca.Library.PicoPlaca.IsPlaqueValid(plaque);
+
+            //Arrange
+            Assert.AreEqual(actual, output);
+        }
+
+        [Test]
+        public void PicoPlaca_WithPlaqueLast4Numbers_ReturnsTrue()
+        {
+            //Assert
+            string plaque = "AAA2222";
+            bool actual = true;
+
+            //Act
+            var output = PicoPlaca.Library.PicoPlaca.IsPlaqueValid(plaque);
+
+            //Arrange
+            Assert.AreEqual(actual, output);
+        }
+    }
+}
